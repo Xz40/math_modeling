1f7b1f1bba66e1e600f02d3992c26c25aad8376b
Xz40 <103625819+Xz40@users.noreply.github.com>
2023-11-04 14:59:14 +0000

    12

--- Exercise/Ex2.py
+++ Exercise/Ex2.py
@@ -1,7 +1,10 @@
-from math import pi
-from Lec import g
+import math
+import Ex1
 B=30
 h=100
-a=pi/3
-print(g)
-V=(g*h)*(math.tan(30)**2)
+E=300
+a=math.pi/3
+V=((Ex1.g*h)*(math.tan(B)**2))/((2*math.cos(a)**2)*(1-math.tan(B)*math.tan(a)))
+print(V**0.5)
+N=(2/math.pi**0.5) * (Ex1.h/(Ex1.k*200)**3/2)*(math.e**-math.e*200*Ex1.k)*math.e**100
+print(N)
---
+++ Exercise/Ex3.py
@@ -0,0 +1,5 @@
+import numpy as np
+a=np.zeros[3,5]
+print(a)
+
+
