56ca0e214a7a7ad1b5474e081909b083ae90820f
Xz40 <103625819+Xz40@users.noreply.github.com>
2023-11-11 16:03:49 +0000

    1

--- Exercise/Ex8.py
+++ Exercise/Ex8.py
@@ -2,9 +2,14 @@ import numpy as np
 m,n=map(int,input().split())
 a=np.zeros((m,n),int)
 c=[]
+d=[]
 for i in range(m):
  for j in range(n):
   a[i,j]=int(input())
+print(a)
 for i in range(m):
-  c.append(a[i,0])
-print(max(c))
+  c=[]
+  for j in range(n):
+    c.append(a[j,i])
+  print(max(c))
+  
