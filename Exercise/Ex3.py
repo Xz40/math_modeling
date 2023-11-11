e7e7d6e4219315decdaeba851b479eb4a5990bca
Xz40 <103625819+Xz40@users.noreply.github.com>
2023-11-11 14:53:41 +0000

    1

--- Exercise/Ex3.py
+++ Exercise/Ex3.py
@@ -1,5 +1,9 @@
 import numpy as np
-a=np.zeros[3,5]
+from Ex1 import g
+a=np.zeros((6,3),int)
+V,x0,y0=map(int,input().split())
+for i in range(6):
+    a[i,0]=i
+    a[i,1]=x0+V*i
+    a[i,2]=y0+V*i-((g*i**2)/2)
 print(a)
-
-
