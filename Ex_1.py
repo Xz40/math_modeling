57f2bae842c9e6391878ed2c027f91f289034b83
Xz40 <103625819+Xz40@users.noreply.github.com>
2023-12-24 15:31:03 +0000

    1

---
+++ Ex_1.py
@@ -0,0 +1,11 @@
+class school_journal:
+    def __init__(self):
+        a.subject = subject
+        b.student = student
+        c.grade_list = grade_list
+    def grade_list(grade):
+        while grade != 0:
+           c.grade_list = grade
+     def printer(a.subject, b.student)
+        print(f'name = {b.student} \n grade list = {c.grade_list}')
+
